c18571ebdd619d1702021350938e314aee4d4cb4
Andreea Ene <ene_andreea@ymail.com>
2016-09-27 21:29:13 +0300

    Cosmetics and comments.

--- WebServerHomework/src/main/java/com/ade/WebServerHomeworkApplication.java
+++ WebServerHomework/src/main/java/com/ade/WebServerHomeworkApplication.java
@@ -6,12 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WebServerHomeworkApplication {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-//        System.setProperty("spring.template.cache", "false");
-//        System.setProperty("spring.thymeleaf.cache", "false");
-		SpringApplication.run(WebServerHomeworkApplication.class, args);
-	}
+        SpringApplication.run(WebServerHomeworkApplication.class, args);
+    }
 }
 
 
