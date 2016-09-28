0e03be4082c19a3450c1e2e042430a8326b1866e
Andreea Ene <ene_andreea@ymail.com>
2016-09-28 22:41:54 +0300

    Layout changes: noUiSlider and Font Awesome added.

--- WebServerHomework/src/main/resources/public/js/control.js
+++ WebServerHomework/src/main/resources/public/js/control.js
@@ -1,1 +1,46 @@
 // TODO: Use jQuery
+
+
+
+$( function () {
+    handleLights();
+    handleTemperature();
+});
+
+
+function handleLights() {
+    var button = $('#lights-button');
+    var formInput = $('#lights-form-input');
+
+    var setClass = function () {
+        button.removeClass('lights-on lights-off');
+        button.addClass(formInput.is(':checked') ?
+            'lights-on' :
+            'lights-off');
+    };
+
+    setClass();  // Initialization
+
+    button.click(function (event) {
+        event.preventDefault();  // Don't send request.
+
+        formInput.attr('checked', !formInput.is(':checked'));  // Invert checked status.
+        setClass();
+    });
+}
+
+function handleTemperature() {
+    var slider = $("#temperature-slider");
+    noUiSlider.create(slider.get(0), {
+        start: 0,
+        tooltips: true,
+        range: {
+            min: -5,
+            max: 30
+        },
+    });
+
+}
+
+
+
